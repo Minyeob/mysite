1aedfc4f243172c3980fb1ca5c2be756ec650b3d
min2 <nmy2511@gmail.com>
2016-09-16 11:27:59 +0000

    currency making

--- currency/forms.py
+++ currency/forms.py
@@ -1,10 +1,60 @@
 from django import forms
 from currency.models import Choice
+from django.shortcuts import render
+from currency.models import CurrencyKRW, CurrencyEUR, Choice, CurrencyModel, CurrencyCNY, CurrencyJPY, CurrencyGBP
+from django.utils import timezone
+import json
+import urllib2
+import datetime
+import unicodedata
 
 class InputValueForm(forms.Form):
-    inputvaluefield = forms.CharField(label="used value")
-    selectcurrencyfield = forms.ChoiceField(widget=forms.RadioSelect, choices=Choice.objects.all())
+    used_amount_field = forms.CharField(label="Used Amount")
+    choice_datas=Choice.objects.all()
+    choice_list=[]
+    for choice in choice_datas:
+        choice_list.append((choice , choice.choice_text))
+    #for choice in choice_datas:
+     #   choice_list.append(choice.choice_text)
+    select_currency_field = forms.ChoiceField(choices=choice_list, widget=forms.Select(), label="Now Currency")
     
     #Control when form is submitted
     def submitted(self, request):
-        a=Choice.objects.last()
+        """used_amount = request.POST['used_amount_field']
+        used_amount_string = unicodedata.normalize('NFKD', used_amount).encode('ascii','ignore')
+        selected_currency = request.POST.get('select_currency_field')
+        selected_currency_string = unicodedata.normalize('NFKD', selected_currency).encode('ascii','ignore')
+        #render(request, 'currency/result.html', {'used_amount':used_amount, 'selected_currency':selected_currency})
+        currencyModel_list = [CurrencyKRW.objects.last(), CurrencyEUR.objects.last(), CurrencyCNY.objects.last(), CurrencyJPY.objects.last(), CurrencyGBP.objects.last()]
+        selected_currency_model = currencyModel_list[0]
+        for currencymodel in currencyModel_list:
+            if currencymodel.nation==selected_currency_string:
+                selected_currency_model = currencymodel
+        lastcurrency = selected_currency_model
+        
+        today=datetime.date.today()
+        if lastcurrency.pub_date==datetime.date.today():
+            lastcurrency.currency_final = CurrencyKRW.objects.last().currency_rate/lastcurrency.currency_rate
+            lastcurrency.save()
+        else:
+            currencyeur = CurrencyEUR()
+            currencykrw = CurrencyKRW()
+            currencycny = CurrencyCNY()
+            currencyjpy = CurrencyJPY()
+            currencygbp = CurrencyGBP()
+            json_object = "https://openexchangerates.org/api/latest.json?app_id=548749f027434cde84582df98bb8df5b"
+            recived_object = urllib2.urlopen(json_object)
+            recived_data = json.loads(recived_object.read())
+            currencyeur.currency_rate = float(recived_data["rates"]["EUR"])
+            currencykrw.currency_rate = float(recived_data["rates"]["KRW"])
+            currencycny.currency_rate = float(recived_data["rates"]["CNY"])
+            currencyjpy.currency_rate = float(recived_data["rates"]["JPY"])
+            currencygbp.currency_rate = float(recived_data["rates"]["GBP"])
+            currencyeur.save()
+            currencykrw.save()
+            currencycny.save()
+            currencyjpy.save()
+            currencygbp.save()
+            lastcurrency.currency_final = CurrencyKRW.objects.last().currency_rate/lastcurrency.currency_rate
+            lastcurrency.save()
+        return render(request, 'currency/result.html', {'currency': lastcurrency})"""
--- currency/views.py
+++ currency/views.py
@@ -13,12 +13,17 @@ import json
 import urllib2
 import datetime
 from currency.forms import InputValueForm
+from django.core.urlresolvers import reverse_lazy
+import unicodedata
 
 class CurrencyIndexView(FormView):
     template_name = 'currency/currencyindex.html'
-    success_url = 'currency:select'
+    success_url = reverse_lazy('currency:select')
     form_class = InputValueForm
     
+    def form_valid(self, form):
+        form.submitted(self.request)
+        return super(CurrencyIndexView, self).form_valid(form)
     
     #When submit request is handled in html
     """def get(self, request):
@@ -26,7 +31,48 @@ class CurrencyIndexView(FormView):
         return render(request, 'currency/index.html', {'choice_list': choice_list})     
     """
 class SelectView(View):
-    def post(self, request):
+    def get(self, request):
+        
+        used_amount = request.POST['used_amount_field']
+        used_amount_string = unicodedata.normalize('NFKD', used_amount).encode('ascii','ignore')
+        selected_currency = request.POST.get('select_currency_field')
+        selected_currency_string = unicodedata.normalize('NFKD', selected_currency).encode('ascii','ignore')
+        #render(request, 'currency/result.html', {'used_amount':used_amount, 'selected_currency':selected_currency})
+        currencyModel_list = [CurrencyKRW.objects.last(), CurrencyEUR.objects.last(), CurrencyCNY.objects.last(), CurrencyJPY.objects.last(), CurrencyGBP.objects.last()]
+        selected_currency_model = currencyModel_list[0]
+        for currencymodel in currencyModel_list:
+            if currencymodel.nation==selected_currency_string:
+                selected_currency_model = currencymodel
+        lastcurrency = selected_currency_model
+        
+        today=datetime.date.today()
+        if lastcurrency.pub_date==datetime.date.today():
+            lastcurrency.currency_final = CurrencyKRW.objects.last().currency_rate/lastcurrency.currency_rate
+            lastcurrency.save()
+        else:
+            currencyeur = CurrencyEUR()
+            currencykrw = CurrencyKRW()
+            currencycny = CurrencyCNY()
+            currencyjpy = CurrencyJPY()
+            currencygbp = CurrencyGBP()
+            json_object = "https://openexchangerates.org/api/latest.json?app_id=548749f027434cde84582df98bb8df5b"
+            recived_object = urllib2.urlopen(json_object)
+            recived_data = json.loads(recived_object.read())
+            currencyeur.currency_rate = float(recived_data["rates"]["EUR"])
+            currencykrw.currency_rate = float(recived_data["rates"]["KRW"])
+            currencycny.currency_rate = float(recived_data["rates"]["CNY"])
+            currencyjpy.currency_rate = float(recived_data["rates"]["JPY"])
+            currencygbp.currency_rate = float(recived_data["rates"]["GBP"])
+            currencyeur.save()
+            currencykrw.save()
+            currencycny.save()
+            currencyjpy.save()
+            currencygbp.save()
+            lastcurrency.currency_final = CurrencyKRW.objects.last().currency_rate/lastcurrency.currency_rate
+            lastcurrency.save()
+        return render(request, 'currency/result.html', {'currency': lastcurrency})
+        
+        """
         #request.POST is object include data in submitted form
         choice_id = request.POST['choice']
         selected_choice = Choice.objects.get(pk=choice_id)
@@ -63,6 +109,6 @@ class SelectView(View):
             lastcurrency.currency_final = CurrencyKRW.objects.last().currency_rate/lastcurrency.currency_rate
             lastcurrency.save()
         return render(request, 'currency/result.html', {'currency': lastcurrency})    
-            
+        """
         
         
